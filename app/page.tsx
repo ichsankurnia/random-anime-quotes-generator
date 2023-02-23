62853bee8355cee70283ccfdc0236dc862d2ee52
Ichsan Kurniawan <43689091+ichsankurnia@users.noreply.github.com>
2023-02-23 14:36:01 +0700

    update style

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import Card from "@/components/Card"
+import UserProfile from "@/components/UserProfile"
 import Link from "next/link"
 
 const ORDER = ['popular', 'latest']
 const CAETGORY = ['backgrounds', 'nature', 'animals', 'places']
-const LIMIT = 10
+const LIMIT = 20
 
 type ResponseData = {
   total: number
@@ -51,14 +52,17 @@ export default async function Home() {
     <div className={`text-white h-screen overflow-hidden flex justify-center items-center p-8 bg-cover bg-center`} style={{ backgroundImage: `url(${bgImage})` }}>
       <Card />
       <div className="fixed top-0 w-full px-3 py-2 md:px-4 2xl:px-5 2xl:py-3 flex justify-between text-sm font-medium">
-        <Link href={photo.pageURL} target="_blank" rel="noopener noreferrer" className="hover:opacity-70 duration-200">Pixaby</Link>
-        <div className="flex items-center space-x-2 text-xs sm:text-sm">
+        <Link href={photo.pageURL} target="_blank" rel="noopener noreferrer" className="hover-duration">Pixabay</Link>
+        {/* <div className="flex items-center space-x-2 text-xs sm:text-sm">
           <i className="fa-solid fa-tags"></i>
           <p className="max-w-[250px] sm:max-w-full truncate">{photo.tags}</p>
-        </div>
+        </div> */}
+        <UserProfile />
       </div>
       <div className="fixed bottom-0 w-full px-3 py-2 md:px-4 2xl:px-5 2xl:py-3 flex justify-between items-center text-sm font-medium">
-        <Link className="max-w-[200px] sm:max-w-full truncate text-xs sm:text-sm" href={photo.userImageURL} target="_blank" rel="noopener noreferrer">Photo by {photo.user}</Link>
+        <Link className="max-w-[200px] sm:max-w-full truncate text-xs sm:text-sm hover-duration" href={photo.userImageURL} target="_blank" rel="noopener noreferrer">
+          Photo by {photo.user}
+        </Link>
         <div className="grid grid-cols-3 gap-2">
           <div className="flex flex-col items-center justify-center">
             <i className="fa-solid fa-eye w-5 h-6 flex justify-center items-center text-base"></i>
--- components/Card.tsx
+++ components/Card.tsx
@@ -34,17 +34,17 @@ const Card: React.FC<Props> = ({ }) => {
                 <div className="backdrop-blur-sm bg-white/30 p-8 rounded-tl-xl rounded-br-xl text-base 2xl:text-lg w-full">
                     <p>Anime: <span className='font-medium'>{quote.anime}</span></p>
                     <p className="font-medium my-7">“{quote.quote}”</p>
-                    <div className="flex item-center space-x-2 font-normal">
+                    <div className="flex flex-col item-center sm:flex-row space-y-2 sm:space-y-0 sm:space-x-2 font-normal">
                         <p>by {quote.character}</p>
-                        <p>|</p>
+                        <p className='hidden sm:block'>|</p>
                         <Link href={`https://twitter.com/intent/tweet?text="${quote.quote} by ${quote.character}"`} target="_blank" rel="noopener noreferrer"
                             className='hover:text-blue-500 duration-300'
                         >
-                            Tweet{' '}<i className="fa-brands fa-twitter hover:rotate-[360deg] duration-300"></i>
+                            Tweet{' '}<i className="fa-brands fa-twitter hover-rotate"></i>
                         </Link>
                     </div>
                 </div>
-                <button type="button" className="mt-2 md:mt-3 inline-block px-6 py-3 font-medium text-sm text-center bg-gradient-to-tl from-blue-600 to-cyan-400 align-middle transition-all rounded-lg cursor-pointer leading-pro ease-soft-in tracking-tight-soft shadow-soft-md bg-150 bg-x-25 hover:scale-102 active:opacity-85 hover:shadow-soft-xs text-white hover:opacity-80 duration-300"
+                <button type="button" className="mt-3 inline-block px-6 py-3 font-medium text-sm text-center bg-gradient-to-tl from-blue-600 to-cyan-400 align-middle transition-all rounded-lg cursor-pointer leading-pro ease-soft-in tracking-tight-soft shadow-soft-md bg-150 bg-x-25 hover:scale-102 active:opacity-85 hover:shadow-soft-xs text-white hover:opacity-80 duration-300"
                     onClick={() => {
                         setLoading(true)
                         getQuote()
